2c7352ed573a8858c1a9dae3e9e52a83b640963d
realaltffour <56314286+realaltffour@users.noreply.github.com>
2020-04-28 14:36:08 +0300

    Remove extra null-termination

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@
 #define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
 
 static const int IGNORE_LIST_SIZE = 1;
-static const char *extension_ignore_list[] = { "crdownload\0" };
+static const char *extension_ignore_list[] = { "crdownload" };
 
 static const char prefix_dir[] = "orgdl";
 
